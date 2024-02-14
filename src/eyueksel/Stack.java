605f1d6547df22f826dc21837e67308813c28f29
Yueksel Eren Nazim <eyueksel@student.tgm.ac.at>
2024-02-14 21:08:11 +0100

    commit 03: push pop and peek methods written

--- src/eyueksel/Stack.java
+++ src/eyueksel/Stack.java
@@ -1,16 +1,45 @@
 package eyueksel;
 
 public class Stack <T> {
-    public void push (T element){
+    private T[] list;
 
+    public Stack() {
+        this.list = (T[])new Object[10];
     }
-    public void pop (){
-
+    public Stack(int fields) {
+        this.list = (T[])new Object[fields];
+    }
+    public void push (T element){
+        for(int i = 0; i < list.length; i++){
+            if(list[i] == null){
+                list[i] = element;
+            }
+        }
+    }
+    public T pop (){
+        T element = null;
+        for(int i = list.length-1; i > 0; i--){
+            if(list[i] != null){
+                element = list[i];
+                list[i] = null;
+            }
+        }
+        return element;
     }
     public T peek (){
-        return null;
+        T element = null;
+        for(int i = list.length-1; i > 0; i--){
+            if(list[i] != null){
+                element = list[i];
+            }
+        }
+        return element;
     }
     public String list(){
-        return null;
+        String text = "";
+        for(int i = 0; i < list.length; i++){
+
+        }
+        return text;
     }
 }
