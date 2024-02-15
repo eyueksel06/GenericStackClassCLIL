88620ba47063286c3471b74b1f2cb878ca54d18d
Yueksel Eren Nazim <eyueksel@student.tgm.ac.at>
2024-02-15 15:10:01 +0100

    commit 04: methods tested and works correctly

--- src/eyueksel/Main.java
+++ src/eyueksel/Main.java
@@ -1,4 +1,32 @@
 package eyueksel;
 
+import java.nio.channels.ScatteringByteChannel;
+
 public class Main {
+    public static void main (String args[]) throws StackFullException {
+        var stringStack = new Stack<String>(5);
+        var intStack = new Stack<Integer>(5);
+        try{
+            stringStack.push("a");
+            stringStack.push("b");
+            stringStack.push("c");
+            stringStack.push("d");
+            stringStack.push("e");
+            // stringStack.push("f");
+            intStack.push(4);
+            intStack.push(8);
+            System.out.println(stringStack.list());
+            System.out.println("First deleted Element of the stringStack: "+stringStack.pop());
+            stringStack.pop();
+            stringStack.pop();
+            stringStack.pop();
+            // stringStack.pop();
+            // stringStack.pop();
+            System.out.println("First deleted Element of the intStack: "+intStack.pop());
+            System.out.println(stringStack.list());
+        }catch(StackFullException | StackEmptyException e){
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/eyueksel/Stack.java
+++ src/eyueksel/Stack.java
@@ -2,28 +2,33 @@ package eyueksel;
 
 public class Stack <T> {
     private T[] list;
+    private int elements = 0;
 
     public Stack() {
-        this.list = (T[])new Object[10];
+        this.list = (T[]) new Object[10];
     }
     public Stack(int fields) {
-        this.list = (T[])new Object[fields];
+        this.list = (T[]) new Object[fields];
     }
-    public void push (T element){
+    public void push (T element) throws StackFullException{
         for(int i = 0; i < list.length; i++){
             if(list[i] == null){
                 list[i] = element;
+                break;
             }
+        }
+        elements++;
+        if(elements > list.length){
+            throw new StackFullException("Stack is full");
         }
     }
-    public T pop (){
-        T element = null;
-        for(int i = list.length-1; i > 0; i--){
-            if(list[i] != null){
-                element = list[i];
-                list[i] = null;
-            }
+    public T pop () throws StackEmptyException{
+        if(list[0] == null){
+            throw new StackEmptyException("The Stack is empty");
         }
+        T element = list[elements-1];
+        list[elements-1] = null;
+        elements--;
         return element;
     }
     public T peek (){
@@ -37,8 +42,8 @@ public class Stack <T> {
     }
     public String list(){
         String text = "";
-        for(int i = 0; i < list.length; i++){
-
+        for(int i = 0; i < elements; i++){
+            text += list[i].toString()+"; ";
         }
         return text;
     }
